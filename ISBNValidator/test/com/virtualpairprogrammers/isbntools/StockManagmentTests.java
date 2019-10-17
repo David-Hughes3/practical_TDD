0e82642661472dbb193de3b0736f2eaf8c9d109f
David Hughes <d.hughes33@live.com>
2019-10-17 02:12:14 -0700

    Ch7 Stubs; add tests and classes associated with Locator Codes

---
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/StockManagmentTests.java
@@ -0,0 +1,28 @@
+package com.virtualpairprogrammers.isbntools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StockManagmentTests {
+	
+	@Test
+	public void testCanGetACorrectLocatorCode() {
+		
+		ExternalISBNDataService testService = new ExternalISBNDataService() {
+			
+			@Override
+			public Book lookup(String isbn) {
+				return new Book(isbn, "Of Mice and Men", "J. Steinbeck"); // test stub
+			}
+		};
+		
+		StockManager stockManager = new StockManager();
+		stockManager.setService(testService);
+		
+		String isbn = "0140177396";
+		String locatorCode = stockManager.getLocatorCode(isbn);
+		assertEquals("7396J4", locatorCode);
+	}
+
+}
