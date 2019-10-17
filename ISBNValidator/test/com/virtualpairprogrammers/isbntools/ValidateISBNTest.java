52a65b2669cc00240135655553bfa8364354eb75
David Hughes <d.hughes33@live.com>
2019-10-17 01:26:41 -0700

    4.10 Finishing the business req; updating ValidateISBN to handle 13 digit isbns

--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -7,25 +7,41 @@ import org.junit.Test;
 public class ValidateISBNTest {
 
 	@Test
-	public void checkValidISBN() {
+	public void checkValid10DigitISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449116");
 		assertTrue("first value", result);
 		result = validator.checkISBN("0140177396");
 		assertTrue("second value", result);
 	}
+	
+	@Test
+	public void checkValid13DigitISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("9781853260087");
+		assertTrue("first value",result);
+		result = validator.checkISBN("9781853267338");
+		assertTrue("second value",result);
+	}
 
 	@Test
-	public void ISBNNumbersEndingInAnXAreValid() {
+	public void TenDigitISBNNumbersEndingInAnXAreValid() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("012000030X");
 		assertTrue(result);
 	}
 	
 	@Test
-	public void checkInvalidISBN() {
+	public void checkInvalid10DigitISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("0140449117");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void checkInvalid13DigitISBN() {
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN("9781853260086");
 		assertFalse(result);
 	}
 	
